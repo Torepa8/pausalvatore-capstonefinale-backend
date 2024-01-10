112b35c64f9b9ce8886721de63926d3c3ded3931
Torepa8 <65725642+Torepa8@users.noreply.github.com>
2024-01-10 19:50:28 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ const port = 3030;
 server.use(express.json());
 
 server.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+    console.log(`Server is listening on port ${port}`);
 });
 
 server.get('/hello', (req, res) => {
-  res.json({messaggio:"Hello Junior Dev"});
+    res.json({ messaggio: "Hello Junior Dev" });
 });
